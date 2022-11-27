ac21693434774d36b7fa648b663bb9afb6d09ee1
dane9640 <gandalf12298@gmail.com>
2022-11-27 03:49:19 -0500

    Inf-Scrolling working
    DOC Block and if the page needs to load if the cards dont cause scrolling

--- js/ajax-api.js
+++ js/ajax-api.js
@@ -3,7 +3,7 @@ Project API:
 omdbapi.com
 http://www.omdbapi.com/?apikey=[yourkey]&
 
-API Key: d00f2a4c
+
 
 Lazy Loading tutorial for API calls:
 https://medium.com/@kennethscoggins/how-to-use-the-infinite-scrolling-method-in-javascript-to-manage-large-api-result-sets-b8f78dba66fb
@@ -18,25 +18,26 @@ submitSearchTerm();
 function submitSearchTerm(){
   
   const SEARCH_BUTTON = document.querySelector('input[type="submit"]');
-  const API_KEY = 'd00f2a4c';
+  const API_KEY = 'f3786fc1';
   const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&`;
+
 
   var page = 1;
   var searchTerm = '';
 
   SEARCH_BUTTON.addEventListener('click', (e) => {
     e.preventDefault();
+    page = 1;
     
     searchTerm = `s=${document.querySelector('#search').value}`;
-    createRequest(`${URL}${searchTerm}&page=${page}`, searchSuccess, searchError);
-    page++;
+
+    createRequest(`${URL}${searchTerm}&page=${page}`, searchSuccess, searchError, e);
   });
 
-  window.addEventListener('scroll', () => {
+  window.addEventListener('scroll', (e) => {
     if(window.innerHeight + window.scrollY > document.body.offsetHeight - 200){
-      console.log(`${URL}${searchTerm}&page=${page}`);
-      createRequest(`${URL}${searchTerm}&page=${page}`, searchSuccess, searchError);
       page++;
+      createRequest(`${URL}${searchTerm}&page=${page}`, searchSuccess, searchError, e);
     }
   });
 }
@@ -45,10 +46,10 @@ function submitSearchTerm(){
  * Creates fetch request
  *
  */
-function createRequest(url, succeed, fail){
+function createRequest(url, succeed, fail, sourceEvent){
   fetch(url)
     .then((response) => handleErrors(response))
-    .then((data) => succeed(data))
+    .then((data) => succeed(data, sourceEvent))
     .catch((error) => fail(error));
 }
 
@@ -68,42 +69,52 @@ function handleErrors(response){
  * Updates DOM on a successful search, including 'no movies found'
  *
  */
-function searchSuccess(data){
+function searchSuccess(data, sourceEvent){
   const SEARCH_TERM = document.querySelector('#search').value
   const HR = document.querySelector('hr');
 
-  var searchSubmitted = false;
-  
+  if(!document.querySelector('h2')){
+    var h2 = document.createElement('h2');
+    HR.insertAdjacentElement('afterend', h2);
+  } else {
+    var h2 = document.querySelector('h2');
+  }
+
+  if(!data.totalResults && sourceEvent.type == 'click'){
+    h2.innerText = `Sorry. No movies found with ${SEARCH_TERM}`;
+  } else if(sourceEvent.type != 'scroll'){
+    h2.innerText = `Total matches for ${SEARCH_TERM}: ${data.totalResults}`;
+  }
+
   if(document.querySelector('#resultSection')){
     var resultSection = document.querySelector('#resultSection');
+    if(sourceEvent.type == 'click'){
+      resultSection.innerHTML = '';
+    }
   } else {
     var resultSection = document.createElement('section');
     resultSection.setAttribute('id', 'resultSection');
-    searchSubmitted = true;
+    h2.insertAdjacentElement('afterend', resultSection);
+    
   }
-
-  var h2 = document.createElement('h2');
-
-  if(!data.totalResults){
-    h2.innerText = `Sorry. No movies found with ${SEARCH_TERM}`;
-    HR.insertAdjacentElement('afterend', h2);
-  } else {
-    h2.innerText = `Total matches for ${SEARCH_TERM}: ${data.totalResults}`;
-    HR.insertAdjacentElement('afterend', h2);
-
+  
+  if (data.totalResults){
     var searchResults = data.Search;
     searchResults.forEach((result, i) => {
       var movieCard = document.createElement('div');
       movieCard.setAttribute('class','movieCard');
       
+      var movieInfo = document.createElement('div');
+      movieInfo.setAttribute('id', 'movieInfo');
       var movieTitle = document.createElement('h3');
       var moviePoster = document.createElement('img');
       var movieYear = document.createElement('p');
 
-      movieTitle.innerText = `${i+1}.${result.Title}`;
-      movieCard.insertAdjacentElement('afterbegin', movieTitle);
+      movieTitle.innerText = `${result.Title}`;
       movieYear.innerText = result.Year;
-      movieCard.insertAdjacentElement('beforeend', movieYear);
+      movieInfo.insertAdjacentElement('beforeend', movieTitle);
+      movieInfo.insertAdjacentElement('beforeend', movieYear);
+      movieCard.insertAdjacentElement('beforeend', movieInfo);
       
       if(result.Poster != 'N/A'){
         moviePoster.setAttribute('src', result.Poster);
@@ -115,7 +126,6 @@ function searchSuccess(data){
       resultSection.insertAdjacentElement('beforeend', movieCard);
     });
   }
-  h2.insertAdjacentElement('afterend', resultSection);
 }
 
 /*
